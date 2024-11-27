991f4a9d0b257dac7a50c359b48ee41820558fe9
gengteng <me@gteng.org>
2024-11-27 10:46:25 +0800

    complete token and token-cli

--- solana/token/src/processor.rs
+++ solana/token/src/processor.rs
@@ -8,6 +8,7 @@ use solana_program::pubkey::Pubkey;
 use solana_program::rent::Rent;
 use solana_program::sysvar::Sysvar;
 use solana_program::{msg, system_instruction};
+use spl_associated_token_account::instruction::create_associated_token_account;
 use spl_token::instruction::initialize_mint;
 use spl_token::state::Mint;
 
@@ -15,7 +16,7 @@ pub struct Processor {}
 
 impl Processor {
     pub fn process(
-        program_id: &Pubkey,
+        _program_id: &Pubkey,
         accounts: &[AccountInfo],
         instruction_data: &[u8],
     ) -> ProgramResult {
@@ -27,7 +28,7 @@ impl Processor {
             }
             TokenInstruction::Mint { amount } => {
                 msg!("Instruction: Mint");
-                Self::mint(accounts, amount)?;
+                Self::mint_token(accounts, amount)?;
             }
         }
         Ok(())
@@ -85,8 +86,72 @@ impl Processor {
         Ok(())
     }
 
-    fn mint(accounts: &[AccountInfo], amount: u64) -> ProgramResult {
-        msg!("Mint");
+    fn mint_token(accounts: &[AccountInfo], amount: u64) -> ProgramResult {
+        let account_iter = &mut accounts.iter();
+        let mint_account = next_account_info(account_iter)?;
+        let associated_token_account = next_account_info(account_iter)?;
+        let rent_sysvar = next_account_info(account_iter)?;
+        let payer = next_account_info(account_iter)?;
+        let system_program = next_account_info(account_iter)?;
+        let token_program = next_account_info(account_iter)?;
+        let ata_program = next_account_info(account_iter)?;
+
+        msg!("ATA is: {}", associated_token_account.key);
+
+        if associated_token_account.lamports() == 0 {
+            msg!("Creating associated token account");
+            let create_account_ix = create_associated_token_account(
+                payer.key,
+                payer.key,
+                mint_account.key,
+                token_program.key,
+            );
+
+            invoke(
+                &create_account_ix,
+                &[
+                    payer.clone(),
+                    associated_token_account.clone(),
+                    mint_account.clone(),
+                    system_program.clone(),
+                    token_program.clone(),
+                    rent_sysvar.clone(),
+                    ata_program.clone(),
+                ],
+            )?;
+        }
+
+        msg!(
+            "Minting {} tokens to ATA {}",
+            amount,
+            associated_token_account.key
+        );
+
+        let mint_ix = spl_token::instruction::mint_to(
+            token_program.key,
+            mint_account.key,
+            associated_token_account.key,
+            payer.key,
+            &[payer.key],
+            amount,
+        )?;
+
+        invoke(
+            &mint_ix,
+            &[
+                mint_account.clone(),
+                payer.clone(),
+                associated_token_account.clone(),
+                token_program.clone(),
+            ],
+        )?;
+
+        msg!(
+            "Minted {} tokens to ATA {}",
+            amount,
+            associated_token_account.key
+        );
+
         Ok(())
     }
 }
